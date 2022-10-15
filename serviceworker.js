533301cc29079b60bb8538a250f2947c793bab72
alin02912 <85516870+alin02912@users.noreply.github.com>
2022-10-14 23:24:03 -0400

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -3,17 +3,19 @@ const urlsToCache = ["index.html", "style.css", "script.js"];
 self.addEventListener("install", event => {
     console.log("Install event triggered");
     event.waitUntil(caches.open("pwa-assets")
-    .then(cache => {
-       return cache.addAll(urlsToCache);
-    }
-    ));
+        .then(cache => {
+            return cache.addAll(urlsToCache);
+        }
+        ));
     console.log("Install event about to finish");
 });
 self.addEventListener("fetch", event => {
+    console.log("Fetch ran!");
     event.respondWith(
         caches.match(event.request).then(
             cacheResponse => cacheResponse || fetch(event.request)
         )
     )
+    console.log("Fetch finished running");
 });
 self.addEventListener("activate", () => console.log("SW Activated!"))
